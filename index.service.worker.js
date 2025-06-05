32fc4a9178667f51f51c7dc9eb31e6bad34ce3e7
embersofintention <embersofintention@pm.me>
2025-06-04 23:12:07 -0400

    fullscreen?

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749092957|2109493631';
+const CACHE_VERSION = '1749093101|2253606823';
 /** @type {string} */
 const CACHE_PREFIX = 'FollowerFriend-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
